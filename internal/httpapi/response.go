c70cef89e24d6845202bdcf66fc73462c158dc74
Eaglesflight <okonkwopaul348@gmail.com>
2026-09-04 11:27:07 +0100

    Restore HTTP API response helpers

---
+++ internal/httpapi/response.go
@@ -0,0 +1,26 @@
+package httpapi
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/google/uuid"
+)
+
+func parseUUIDPathValue(r *http.Request, name string) (uuid.UUID, error) {
+	return uuid.Parse(r.PathValue(name))
+}
+
+func writeJSON(w http.ResponseWriter, status int, value any) {
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.WriteHeader(status)
+	_ = json.NewEncoder(w).Encode(value)
+}
+
+type errorResponse struct {
+	Error string `json:"error"`
+}
+
+func writeError(w http.ResponseWriter, status int, message string) {
+	writeJSON(w, status, errorResponse{Error: message})
+}
